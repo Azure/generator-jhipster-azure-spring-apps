99e8706eaf2a6fbe2e495d97ff3b8f48717c7c9a
sinedied <noda@free.fr>
2023-04-19 08:34:55 +0200

    Fix formatting

--- generators/createtodoapp/generator.mjs
+++ generators/createtodoapp/generator.mjs
@@ -1,11 +1,6 @@
 import chalk from 'chalk';
 import BaseGenerator from 'generator-jhipster/esm/generators/base';
-import {
-  PRIORITY_PREFIX,
-  PROMPTING_PRIORITY,
-  WRITING_PRIORITY,
-  END_PRIORITY,
-} from 'generator-jhipster/esm/priorities';
+import { PRIORITY_PREFIX, PROMPTING_PRIORITY, WRITING_PRIORITY, END_PRIORITY } from 'generator-jhipster/esm/priorities';
 
 export default class extends BaseGenerator {
   constructor(args, opts, features) {
@@ -15,34 +10,30 @@ export default class extends BaseGenerator {
   get [PROMPTING_PRIORITY]() {
     return {
       async promptingTemplateTask() {
-
         const prompts = [
           {
-            type: "input",
-            name: "applicationName",
-            message: "What is your application name?",
-            default: "azure-spring-apps-todo"
+            type: 'input',
+            name: 'applicationName',
+            message: 'What is your application name?',
+            default: 'azure-spring-apps-todo',
           },
           {
-            type: "input",
-            name: "serverPort",
-            message: "On which port would you like your server to run?",
-            default: 8080
+            type: 'input',
+            name: 'serverPort',
+            message: 'On which port would you like your server to run?',
+            default: 8080,
           },
           {
-            type: "input",
-            name: "packageName",
-            message: "What is your default java package name?",
-            default: "com.mycompany.myapp"
-          }
+            type: 'input',
+            name: 'packageName',
+            message: 'What is your default java package name?',
+            default: 'com.mycompany.myapp',
+          },
         ];
 
-        const props = await this.prompt(
-          prompts,
-          this.blueprintStorage
-        );
+        const props = await this.prompt(prompts, this.blueprintStorage);
 
-        this.todoAppProps = props
+        this.todoAppProps = props;
       },
     };
   }
@@ -50,87 +41,91 @@ export default class extends BaseGenerator {
   get [WRITING_PRIORITY]() {
     return {
       async writingTemplateTask() {
+        this.fs.copy(this.templatePath('client/'), this.destinationPath('client/'));
 
-        this.fs.copy(this.templatePath("client/"), this.destinationPath("client/"),);
-
-        const packageName = this.todoAppProps.packageName.replaceAll(".", "/");
+        const packageName = this.todoAppProps.packageName.replaceAll('.', '/');
 
         await this.writeFiles({
           sections: {
-            maven: [{
-              templates: [
-                { file: 'mvnw', noEjs: true },
-                { file: 'mvnw.cmd', noEjs: true },
-                { file: '.mvn/wrapper/maven-wrapper.jar', noEjs: true },
-                { file: '.mvn/wrapper/maven-wrapper.properties', noEjs: true },
-              ],
-            }],
-            client: [{
-              templates: [
-                {
-                  file: 'clientpom.xml',
-                  renameTo: ctx => `client/pom.xml`
-                },
-              ],
-            }],
-            web: [{
-              templates: [
-                { file: 'web/pom.xml'},
-                {
-                  file: 'web/configuration/RFC3339DateFormat.java',
-                  renameTo: ctx => `web/src/main/java/${packageName}/configuration/RFC3339DateFormat.java`
-                },
-                {
-                  file: 'web/configuration/WebConfiguration.java',
-                  renameTo: ctx => `web/src/main/java/${packageName}/configuration/WebConfiguration.java`
-                },
-                {
-                  file: 'web/model/TodoItem.java',
-                  renameTo: ctx => `web/src/main/java/${packageName}/model/TodoItem.java`
-                },
-                {
-                  file: 'web/model/TodoList.java',
-                  renameTo: ctx => `web/src/main/java/${packageName}/model/TodoList.java`
-                },
-                {
-                  file: 'web/model/TodoState.java',
-                  renameTo: ctx => `web/src/main/java/${packageName}/model/TodoState.java`
-                },
-                {
-                  file: 'web/repository/TodoItemRepository.java',
-                  renameTo: ctx => `web/src/main/java/${packageName}/repository/TodoItemRepository.java`
-                },
-                {
-                  file: 'web/repository/TodoListRepository.java',
-                  renameTo: ctx => `web/src/main/java/${packageName}/repository/TodoListRepository.java`
-                },
-                {
-                  file: 'web/web/HomeController.java',
-                  renameTo: ctx => `web/src/main/java/${packageName}/web/HomeController.java`
-                },
-                {
-                  file: 'web/web/TodoListsController.java',
-                  renameTo: ctx => `web/src/main/java/${packageName}/web/TodoListsController.java`
-                },
-                {
-                  file: 'web/SimpleTodoApplication.java',
-                  renameTo: ctx => `web/src/main/java/${packageName}/SimpleTodoApplication.java`
-                },
-                {
-                  file: 'web/application.yml',
-                  renameTo: ctx => `web/src/main/resources/application.yml`
-                },
-              ],
-            }],
-            parent: [{
-              templates: [
-                { file: 'pom.xml'},
-              ],
-            }]
+            maven: [
+              {
+                templates: [
+                  { file: 'mvnw', noEjs: true },
+                  { file: 'mvnw.cmd', noEjs: true },
+                  { file: '.mvn/wrapper/maven-wrapper.jar', noEjs: true },
+                  { file: '.mvn/wrapper/maven-wrapper.properties', noEjs: true },
+                ],
+              },
+            ],
+            client: [
+              {
+                templates: [
+                  {
+                    file: 'clientpom.xml',
+                    renameTo: ctx => `client/pom.xml`,
+                  },
+                ],
+              },
+            ],
+            web: [
+              {
+                templates: [
+                  { file: 'web/pom.xml' },
+                  {
+                    file: 'web/configuration/RFC3339DateFormat.java',
+                    renameTo: ctx => `web/src/main/java/${packageName}/configuration/RFC3339DateFormat.java`,
+                  },
+                  {
+                    file: 'web/configuration/WebConfiguration.java',
+                    renameTo: ctx => `web/src/main/java/${packageName}/configuration/WebConfiguration.java`,
+                  },
+                  {
+                    file: 'web/model/TodoItem.java',
+                    renameTo: ctx => `web/src/main/java/${packageName}/model/TodoItem.java`,
+                  },
+                  {
+                    file: 'web/model/TodoList.java',
+                    renameTo: ctx => `web/src/main/java/${packageName}/model/TodoList.java`,
+                  },
+                  {
+                    file: 'web/model/TodoState.java',
+                    renameTo: ctx => `web/src/main/java/${packageName}/model/TodoState.java`,
+                  },
+                  {
+                    file: 'web/repository/TodoItemRepository.java',
+                    renameTo: ctx => `web/src/main/java/${packageName}/repository/TodoItemRepository.java`,
+                  },
+                  {
+                    file: 'web/repository/TodoListRepository.java',
+                    renameTo: ctx => `web/src/main/java/${packageName}/repository/TodoListRepository.java`,
+                  },
+                  {
+                    file: 'web/web/HomeController.java',
+                    renameTo: ctx => `web/src/main/java/${packageName}/web/HomeController.java`,
+                  },
+                  {
+                    file: 'web/web/TodoListsController.java',
+                    renameTo: ctx => `web/src/main/java/${packageName}/web/TodoListsController.java`,
+                  },
+                  {
+                    file: 'web/SimpleTodoApplication.java',
+                    renameTo: ctx => `web/src/main/java/${packageName}/SimpleTodoApplication.java`,
+                  },
+                  {
+                    file: 'web/application.yml',
+                    renameTo: ctx => `web/src/main/resources/application.yml`,
+                  },
+                ],
+              },
+            ],
+            parent: [
+              {
+                templates: [{ file: 'pom.xml' }],
+              },
+            ],
           },
-          context: this.todoAppProps
+          context: this.todoAppProps,
         });
-
       },
     };
   }
@@ -138,7 +133,7 @@ export default class extends BaseGenerator {
   get [END_PRIORITY]() {
     return {
       afterRunHook() {
-          this.log(`
+        this.log(`
 ${chalk.greenBright('The TODO template has been created successfully! 🎉')}
 
 ${chalk.magentaBright(`Run locally:`)}
