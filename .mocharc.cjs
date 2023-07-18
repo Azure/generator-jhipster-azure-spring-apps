d1b75983826312ead2c12bde670f1c68e9bad3d2
sinedied <noda@free.fr>
2023-07-18 16:09:54 +0200

    test: set retries in config

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   reporter: "spec",
   slow: 0,
   timeout: 30000,
+  retries: 2,
   ui: "bdd",
   extension: ["spec.js", "spec.cjs", "spec.mjs"],
   exclude: ["**/resources/**", "**/node_modules/**"],
